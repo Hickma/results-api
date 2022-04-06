a5b388d0177b9d5f950be7b386ee8b934589b548
hickma <hickmat108@gmail.com>
2022-04-06 15:48:41 +0100

    first commit

---
+++ results.js
@@ -0,0 +1,12 @@
+const results=[
+    {name:"Windy Dari", class:"form one", marks:8},
+    {name:"Numbu Moomen", class:"form one", marks:6},
+    {name:"Bushiran Witol", class:"form one", marks:7},
+    {name:"Prosper Lucy", class:"form one", marks:3},
+    {name:"Aleem Dari", class:"form one", marks:4},
+    {name:"Suraya Abu", class:"form one", marks:2},
+    {name:"Hickmat Bagmaara", class:"form one", marks:1},
+    
+]
+
+module.exports=results
